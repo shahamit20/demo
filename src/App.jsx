1537072eb238e0dc7859f846b952443fea84f002
shahamit20 <shahamits123@gmail.com>
2025-07-16 12:10:05 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
                   Dropdown
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">amit</a></li>
                   <li><a class="dropdown-item" href="#">Another action</a></li>
             
                   <li><a class="dropdown-item" href="#">Something else here</a></li>
